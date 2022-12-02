9b136be4da0e7c22ff94a9f6df26d19cfdd6f602
jrubialesv <jrubialesv@gmail.com>
2022-12-02 14:44:26 +0100

    Update values

---
+++ src/components/Skull.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>Skull</h1>
+        <p>{{ msg }}</p>
+        <button type="button" class="btn btn-secondary">
+            Skull button
+        </button>
+
+    </div>
+</template>
+ 
+<script>
+import axios from 'axios'
+export default
+{
+    name: 'Skull',
+    data()
+    {
+        return {
+            msg: 'Hi! This is the skull component 💀'
+        }
+    },
+    methods:
+    {
+        getSkull()
+        {
+            axios.get('${process.env.VUE_APP_ROOT_URL}/skull')
+            .then(response => (this.msg = response.data))
+            .catch(error => console.log(error))
+        }
+    },
+    created()
+    {
+        this.getSkull()
+    }
+
+    
+}
+</script>
